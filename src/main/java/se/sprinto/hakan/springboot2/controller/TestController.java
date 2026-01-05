24b549dfa1a92823fcbc26d75c6a0236fd6103ad
hakan-gleissman <hakan.gleissman@gmail.com>
2026-01-05 17:29:33 +0100

    new Controller

---
+++ src/main/java/se/sprinto/hakan/springboot2/controller/TestController.java
@@ -0,0 +1,15 @@
+package se.sprinto.hakan.springboot2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping
+    public String test() {
+
+        return "Hello from Spring Boot updated 3!";
+    }
+}
